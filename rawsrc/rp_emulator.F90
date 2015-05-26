b3ba9def95bdbff3f8222926244d802c1c3a822a
Andrew Dawson <dawson@atm.ox.ac.uk>
2015-05-26 12:12:14 +0100

    Add interfaces for initialization.
    Other changes include commenting of existing subroutines.

--- rawsrc/rp_emulator.F90
+++ rawsrc/rp_emulator.F90
@@ -1,7 +1,7 @@
 MODULE rp_emulator
 ! A reduced-precision emulator.
 !
-! The reduced-precision emulator provides two new data types, `rpe_var`
+! The reduced-precision emulator provides two new data types: `rpe_var`
 ! and `rpe_shadow`. Both will perform calculations with the specified
 ! reduced precision.
 !
@@ -102,23 +102,30 @@ MODULE rp_emulator
         ! Required definitions of abstract methods on the base class:
         PROCEDURE, PUBLIC  :: get_value => get_shadow_value
         PROCEDURE, PRIVATE :: set_value => set_shadow_value
-        ! An initialization method is required to associated the pointer
-        ! with a particular variable:
-        PROCEDURE, PUBLIC  :: initialize => init_shadow_scalar
     END TYPE
+
+    INTERFACE init_shadow
+    ! Interfaces for initializing `rpe_shadow` instances.
+    !
+        MODULE PROCEDURE init_shadow_scalar
+        MODULE PROCEDURE init_shadow_v1d
+        MODULE PROCEDURE init_shadow_v2d
+        MODULE PROCEDURE init_shadow_v3d
+        MODULE PROCEDURE init_shadow_v4d
+    END INTERFACE
 
     INTERFACE ASSIGNMENT(=)
     ! Interfaces for the assignment operator with `rpe_type` instances.
     !
         MODULE PROCEDURE assign_rpe_rpe
         MODULE PROCEDURE assign_rpe_real
-        MODULE PROCEDURE assign_rpe_alternate, &
-                         assign_rpe_integer, &
-                         assign_rpe_long, &
-                         assign_real_rpe, &
-                         assign_alternate_rpe, &
-                         assign_integer_rpe, &
-                         assign_long_rpe
+        MODULE PROCEDURE assign_rpe_alternate
+        MODULE PROCEDURE assign_rpe_integer
+        MODULE PROCEDURE assign_rpe_long
+        MODULE PROCEDURE assign_real_rpe
+        MODULE PROCEDURE assign_alternate_rpe
+        MODULE PROCEDURE assign_integer_rpe
+        MODULE PROCEDURE assign_long_rpe
     END INTERFACE
 
 !-----------------------------------------------------------------------
@@ -136,7 +143,7 @@ MODULE rp_emulator
 CONTAINS
 
 !-----------------------------------------------------------------------
-! Method definitions for rpe_type subclasses:
+! Type-bound procedure definitions for rpe_type subclasses:
 !-----------------------------------------------------------------------
 
     PURE FUNCTION get_var_value (this) result (x)
@@ -159,6 +166,15 @@ CONTAINS
 
     PURE SUBROUTINE set_var_value (this, x)
     ! Setter method for the value contained in an rpe_var instance.
+    !
+    ! Arguments:
+    !
+    ! * this: type(rpe_var) [input/output]
+    !       An `rpe_var` instance to set the value of.
+    !
+    ! * x: polymorphic [input]
+    !       A value to assign to `this`, can be a real or integer type
+    !       or an `rpe_type` instance.
     !
         CLASS(rpe_var), INTENT(INOUT) :: this
         CLASS(*),       INTENT(IN)    :: x
@@ -197,6 +213,15 @@ CONTAINS
     PURE SUBROUTINE set_shadow_value (this, x)
     ! Setter method for the value contained in an rpe_shadow instance.
     !
+    ! Arguments:
+    !
+    ! * this: type(rpe_shadow) [input/output]
+    !       An `rpe_shadow` instance to set the value of.
+    !
+    ! * x: polymorphic [input]
+    !       A value to assign to `this`, can be a real or integer type
+    !       or an `rpe_type` instance.
+    !
         CLASS(rpe_shadow), INTENT(INOUT) :: this
         CLASS(*),          INTENT(IN)    :: x
         SELECT TYPE (x)
@@ -213,18 +238,120 @@ CONTAINS
         END SELECT
     END SUBROUTINE set_shadow_value
 
+!-----------------------------------------------------------------------
+! Interface members for rpe_shadow instance initialization:
+!-----------------------------------------------------------------------
+
     SUBROUTINE init_shadow_scalar (this, x)
-        CLASS(rpe_shadow),                INTENT(OUT) :: this
+    ! Initialization for scalar `rpe_type` instances.
+    !
+    ! Arguments:
+    !
+    ! * this: type(rpe_shadow) [output]
+    !       An `rpe_shadow` instance to initialize.
+    !
+    ! * x: real(kind=RPE_REAL_KIND) [input]
+    !       A real-valued variable to shadow with `this`.
+    !
+        TYPE(rpe_shadow),                INTENT(OUT) :: this
         REAL(KIND=RPE_REAL_KIND), TARGET, INTENT(IN)  :: x
         this%ptr => x
     END SUBROUTINE init_shadow_scalar
 
+    SUBROUTINE init_shadow_v1d (this, x)
+    ! Initialization for 1d-vector `rpe_type` instances.
+    !
+    ! Arguments:
+    !
+    ! * this: type(rpe_shadow), dimension(:) [output]
+    !       An `rpe_shadow` instance to initialize.
+    !
+    ! * x: real(kind=RPE_REAL_KIND), dimension(:) [input]
+    !       A real-valued variable to shadow with `this`.
+    !
+        TYPE(rpe_shadow),         DIMENSION(:),         INTENT(OUT) :: this
+        REAL(KIND=RPE_REAL_KIND), DIMENSION(:), TARGET, INTENT(IN)  :: x
+        INTEGER :: i
+        DO i = 1, SIZE(x, 1)
+            this(i)%ptr => x(i)
+        END DO
+    END SUBROUTINE init_shadow_v1d
+
+    SUBROUTINE init_shadow_v2d (this, x)
+    ! Initialization for 2d-vector `rpe_type` instances.
+    !
+    ! Arguments:
+    !
+    ! * this: type(rpe_shadow), dimension(:, :) [output]
+    !       An `rpe_shadow` instance to initialize.
+    !
+    ! * x: real(kind=RPE_REAL_KIND), dimension(:, :) [input]
+    !       A real-valued variable to shadow with `this`.
+    !
+        TYPE(rpe_shadow),         DIMENSION(:, :),         INTENT(OUT) :: this
+        REAL(KIND=RPE_REAL_KIND), DIMENSION(:, :), TARGET, INTENT(IN)  :: x
+        INTEGER :: i, j
+        DO i = 1, SIZE(x, 1)
+            DO j = 1, SIZE(x, 2)
+                this(i, j)%ptr => x(i, j)
+            END DO
+        END DO
+    END SUBROUTINE init_shadow_v2d
+
+    SUBROUTINE init_shadow_v3d (this, x)
+    ! Initialization for 3d-vector `rpe_type` instances.
+    !
+    ! Arguments:
+    !
+    ! * this: type(rpe_shadow), dimension(:, :, :) [output]
+    !       An `rpe_shadow` instance to initialize.
+    !
+    ! * x: real(kind=RPE_REAL_KIND), dimension(:, :, :) [input]
+    !       A real-valued variable to shadow with `this`.
+    !
+        TYPE(rpe_shadow),         DIMENSION(:, :, :),         INTENT(OUT) :: this
+        REAL(KIND=RPE_REAL_KIND), DIMENSION(:, :, :), TARGET, INTENT(IN)  :: x
+        INTEGER :: i, j, k
+        DO i = 1, SIZE(x, 1)
+            DO j = 1, SIZE(x, 2)
+                DO k = 1, SIZE(x, 3)
+                    this(i, j, k)%ptr => x(i, j, k)
+                END DO
+            END DO
+        END DO
+    END SUBROUTINE init_shadow_v3d
+
+    SUBROUTINE init_shadow_v4d (this, x)
+    ! Initialization for 4d-vector `rpe_type` instances.
+    !
+    ! Arguments:
+    !
+    ! * this: type(rpe_shadow), dimension(:, :, :, :) [output]
+    !       An `rpe_shadow` instance to initialize.
+    !
+    ! * x: real(kind=RPE_REAL_KIND), dimension(:, :, :, :) [input]
+    !       A real-valued variable to shadow with `this`.
+    !
+        TYPE(rpe_shadow),         DIMENSION(:, :, :, :),         INTENT(OUT) :: this
+        REAL(KIND=RPE_REAL_KIND), DIMENSION(:, :, :, :), TARGET, INTENT(IN)  :: x
+        INTEGER :: i, j, k, l
+        DO i = 1, SIZE(x, 1)
+            DO j = 1, SIZE(x, 2)
+                DO k = 1, SIZE(x, 3)
+                    DO l = 1, SIZE(x, 4)
+                        this(i, j, k, l)%ptr => x(i, j, k, l)
+                    END DO
+                END DO
+            END DO
+        END DO
+    END SUBROUTINE init_shadow_v4d
+
 !-----------------------------------------------------------------------
-! Core emulator routines:
+! Core emulator procedures:
 !-----------------------------------------------------------------------
 
     ELEMENTAL SUBROUTINE reduce_precision (x)
-    ! Reduce the precision of a real number.
+    ! Reduce the precision of a `rpe_type` instance.
     !
     ! Truncates the given floating-point number significand to the
     ! number of bits defined by the module variable RPE_BITS. If the
@@ -232,7 +359,7 @@ CONTAINS
     ! unaltered input value, it only performs the bit truncation if
     ! RPE_ACTIVE is true.
     !
-    ! **Argument:**
+    ! Argument:
     !
     ! *x: type(rpe_type) [input/output]
     !     The `rpe_type` instance to truncate.
@@ -275,54 +402,144 @@ CONTAINS
 !-----------------------------------------------------------------------
 
     ELEMENTAL SUBROUTINE assign_rpe_rpe (r1, r2)
+    ! Assign an `rpe_type` instance to another `rpe_type` instance.
+    !
+    ! Arguments:
+    !
+    ! * r1: class(rpe_type) [input/output]
+    !       An `rpe_type` instance to assign to.
+    !
+    ! * r2: class(rpe_type) [input]
+    !       An `rpe_type` instance whose value will be assigned to `r1`.
+    !
         CLASS(rpe_type), INTENT(INOUT) :: r1
         CLASS(rpe_type), INTENT(IN)    :: r2
         CALL r1%set_value(r2%get_value())
     END SUBROUTINE assign_rpe_rpe
 
     ELEMENTAL SUBROUTINE assign_rpe_real (rpe, x)
+    ! Assign a real variable to an `rpe_type` instance.
+    !
+    ! Arguments:
+    !
+    ! * rpe: class(rpe_type) [input/output]
+    !       An `rpe_type` instance to assign to.
+    !
+    ! * x: real(kind=RPE_REAL_KIND) [input]
+    !       A real variable whose value will be assigned to `rpe`.
+    !
         CLASS(rpe_type),          INTENT(INOUT) :: rpe
         REAL(KIND=RPE_REAL_KIND), INTENT(IN)    :: x
         CALL rpe%set_value(x)
     END SUBROUTINE assign_rpe_real
 
     ELEMENTAL SUBROUTINE assign_rpe_alternate (rpe, x)
+    ! Assign a real variable to an `rpe_type` instance.
+    !
+    ! Arguments:
+    !
+    ! * rpe: class(rpe_type) [input/output]
+    !       An `rpe_type` instance to assign to.
+    !
+    ! * x: real(kind=RPE_ALTERNATE_KIND) [input]
+    !       A real variable whose value will be assigned to `rpe`.
+    !
         CLASS(rpe_type),               INTENT(INOUT) :: rpe
         REAL(KIND=RPE_ALTERNATE_KIND), INTENT(IN)    :: x
         CALL rpe%set_value(x)
     END SUBROUTINE assign_rpe_alternate
 
     ELEMENTAL SUBROUTINE assign_rpe_integer (rpe, x)
+    ! Assign an integer variable to an `rpe_type` instance.
+    !
+    ! Arguments:
+    !
+    ! * rpe: class(rpe_type) [input/output]
+    !       An `rpe_type` instance to assign to.
+    !
+    ! * x: integer(kind=4) [input]
+    !       An integer variable whose value will be assigned to `rpe`.
+    !
         CLASS(rpe_type), INTENT(INOUT) :: rpe
         INTEGER(KIND=4), INTENT(IN)    :: x
         CALL rpe%set_value(x)
     END SUBROUTINE assign_rpe_integer
 
     ELEMENTAL SUBROUTINE assign_rpe_long (rpe, x)
+    ! Assign a long integer variable to an `rpe_type` instance.
+    !
+    ! Arguments:
+    !
+    ! * rpe: class(rpe_type) [input/output]
+    !       An `rpe_type` instance to assign to.
+    !
+    ! * x: integer(kind=8) [input]
+    !       A long integer variable whose value will be assigned to `rpe`.
+    !
         CLASS(rpe_type), INTENT(INOUT) :: rpe
         INTEGER(KIND=8), INTENT(IN)    :: x
         CALL rpe%set_value(x)
     END SUBROUTINE assign_rpe_long
 
     ELEMENTAL SUBROUTINE assign_real_rpe (x, rpe)
+    ! Assign an `rpe_type` instance to a real variable.
+    !
+    ! Arguments:
+    !
+    ! * x: real(kind=RPE_REAL_KIND) [input/output]
+    !       A real variable assign to.
+    !
+    ! * rpe: class(rpe_type) [input]
+    !       An `rpe_type` instance whose value will be assigned to `x`.
+    !
         REAL(KIND=RPE_REAL_KIND), INTENT(INOUT) :: x
         CLASS(rpe_type),          INTENT(IN)    :: rpe
         x = rpe%get_value()
     END SUBROUTINE assign_real_rpe
 
     ELEMENTAL SUBROUTINE assign_alternate_rpe (x, rpe)
+    ! Assign an `rpe_type` instance to a real variable.
+    !
+    ! Arguments:
+    !
+    ! * x: real(kind=RPE_ALTERNATE_KIND) [input/output]
+    !       A real variable assign to.
+    !
+    ! * rpe: class(rpe_type) [input]
+    !       An `rpe_type` instance whose value will be assigned to `x`.
+    !
         REAL(KIND=RPE_ALTERNATE_KIND), INTENT(INOUT) :: x
         CLASS(rpe_type),               INTENT(IN)    :: rpe
         x = rpe%get_value()
     END SUBROUTINE assign_alternate_rpe
 
     ELEMENTAL SUBROUTINE assign_integer_rpe (x, rpe)
+    ! Assign an `rpe_type` instance to an integer variable.
+    !
+    ! Arguments:
+    !
+    ! * x: integer(kind=4) [input/output]
+    !       An integer variable assign to.
+    !
+    ! * rpe: class(rpe_type) [input]
+    !       An `rpe_type` instance whose value will be assigned to `x`.
+    !
         INTEGER(KIND=4), INTENT(INOUT) :: x
         CLASS(rpe_type), INTENT(IN)    :: rpe
         x = rpe%get_value()
     END SUBROUTINE assign_integer_rpe
 
     ELEMENTAL SUBROUTINE assign_long_rpe (x, rpe)
+    ! Assign an `rpe_type` instance to a long integer variable.
+    !
+    ! Arguments:
+    !
+    ! * x: integer(kind=8) [input/output]
+    !       A long integer variable assign to.
+    !
+    ! * rpe: class(rpe_type) [input]
+    !       An `rpe_type` instance whose value will be assigned to `x`.
+    !
         INTEGER(KIND=8), INTENT(INOUT) :: x
         CLASS(rpe_type), INTENT(IN)    :: rpe
         x = rpe%get_value()
